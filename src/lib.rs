81f535c18192c85fee5e8988bd615908a56b7ac4
Tony Arcieri <tony@iqlusion.io>
2020-10-17 11:19:46 -0700

    v0.9.0-rc3

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Tendermint Key Management System
 
-#![doc(html_root_url = "https://docs.rs/tmkms/0.9.0-rc2")]
+#![doc(html_root_url = "https://docs.rs/tmkms/0.9.0-rc3")]
 #![forbid(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
 
