b3af866caf691f0e5f23ab2ef6353254daf57020
Jim McDonald <Jim@mcdee.net>
2022-02-23 09:58:49 +0000

    Add test.

---
+++ tests/cli/version.rs
@@ -0,0 +1,15 @@
+//! Integration tests for the `version` subcommand
+
+use crate::cli;
+use std::{ffi::OsStr, str};
+
+#[test]
+fn test_versionx() {
+    let result = cli::run(&[
+        OsStr::new("version"),
+    ]);
+
+    assert!(result.status.success());
+    let stdout = str::from_utf8(&result.stdout).unwrap().trim().to_owned();
+    assert!(stdout.eq(option_env!("CARGO_PKG_VERSION").unwrap_or("unknown")));
+}
