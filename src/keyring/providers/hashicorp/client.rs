0ed3e8313ffe5aa2ded7e2f6ade72b425c29ef67
Serguei Oleinik <serguei.oleinik@figment.io>
2022-08-23 17:51:02 +0300

    keyring tests

--- src/keyring/providers/hashicorp/client.rs
+++ src/keyring/providers/hashicorp/client.rs
@@ -188,3 +188,171 @@ impl TendermintValidatorApp {
         Ok(array)
     }
 }
+
+#[cfg(feature = "hashicorp")]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use base64;
+    use mockito::{mock, server_address};
+
+    const TEST_TOKEN: &str = "test-token";
+    const TEST_KEY_NAME: &str = "test-key-name";
+    const TEST_PUB_KEY_VALUE: &str = "ng+ab41LawVupIXX3ocMn+AfV2W1DEMCfjAdtrwXND8="; //base64
+    const TEST_PAYLOAD_TO_SIGN_BASE64: &str = "cXFxcXFxcXFxcXFxcXFxcXFxcXE="; //$(base64 <<< "qqqqqqqqqqqqqqqqqqqq") => "cXFxcXFxcXFxcXFxcXFxcXFxcXEK", 'K' vs "=" ????
+    const TEST_PAYLOAD_TO_SIGN: &[u8] = b"qqqqqqqqqqqqqqqqqqqq";
+
+    const TEST_SIGNATURE:&str = /*vault:v1:*/ "pNcc/FAUu+Ta7itVegaMUMGqXYkzE777y3kOe8AtdRTgLbA8eFnrKbbX/m7zoiC+vArsIUJ1aMCEDRjDK3ZsBg==";
+
+    #[test]
+    fn hashicorp_connect_ok() {
+        //setup
+        let _lookup_self = mock("GET", "/v1/auth/token/lookup-self")
+            .match_header("X-Vault-Token", TEST_TOKEN)
+            .with_body(TOKEN_DATA)
+            .create();
+
+        //test
+        let app = TendermintValidatorApp::connect(
+            &format!("http://{}", server_address()),
+            TEST_TOKEN,
+            TEST_KEY_NAME,
+        );
+
+        assert!(app.is_ok());
+    }
+
+    #[test]
+    fn hashicorp_public_key_ok() {
+        //setup
+        let _lookup_self = mock("GET", "/v1/auth/token/lookup-self")
+            .match_header("X-Vault-Token", TEST_TOKEN)
+            .with_body(TOKEN_DATA)
+            .create();
+
+        //app
+        let mut app = TendermintValidatorApp::connect(
+            &format!("http://{}", server_address()),
+            TEST_TOKEN,
+            TEST_KEY_NAME,
+        )
+        .expect("Failed to connect");
+
+        //Vault call
+        let read_key = mock(
+            "GET",
+            format!("/v1/transit/keys/{}", TEST_KEY_NAME).as_str(),
+        )
+        .match_header("X-Vault-Token", TEST_TOKEN)
+        .with_body(READ_KEY_RESP)
+        .expect_at_most(1) //one call only
+        .create();
+
+        //server call
+        let res = app.public_key();
+        assert!(res.is_ok());
+        assert_eq!(
+            res.unwrap(),
+            base64::decode(TEST_PUB_KEY_VALUE).unwrap().as_slice()
+        );
+
+        //cached vaule
+        let res = app.public_key();
+        assert!(res.is_ok());
+        assert_eq!(
+            res.unwrap(),
+            base64::decode(TEST_PUB_KEY_VALUE).unwrap().as_slice()
+        );
+
+        read_key.assert();
+    }
+
+    #[test]
+    fn hashicorp_sign_ok() {
+        //setup
+        let _lookup_self = mock("GET", "/v1/auth/token/lookup-self")
+            .match_header("X-Vault-Token", TEST_TOKEN)
+            .with_body(TOKEN_DATA)
+            .create();
+
+        //app
+        let app = TendermintValidatorApp::connect(
+            &format!("http://{}", server_address()),
+            TEST_TOKEN,
+            TEST_KEY_NAME,
+        )
+        .expect("Failed to connect");
+
+        let body = serde_json::to_string(&SignRequest {
+            input: TEST_PAYLOAD_TO_SIGN_BASE64.into(),
+        })
+        .unwrap();
+
+        let _sign_mock = mock(
+            "POST",
+            format!("/v1/transit/sign/{}", TEST_KEY_NAME).as_str(),
+        )
+        .match_header("X-Vault-Token", TEST_TOKEN)
+        .match_body(body.as_str())
+        .with_body(SIGN_RESPONSE)
+        .create();
+
+        //server call
+        let res = app.sign(TEST_PAYLOAD_TO_SIGN);
+        assert!(res.is_ok());
+        assert_eq!(
+            res.unwrap(),
+            base64::decode(TEST_SIGNATURE).unwrap().as_slice()
+        );
+    }
+
+    #[test]
+    fn hashicorp_sign_empty_payload_should_fail() {
+        //setup
+        let _lookup_self = mock("GET", "/v1/auth/token/lookup-self")
+            .match_header("X-Vault-Token", TEST_TOKEN)
+            .with_body(TOKEN_DATA)
+            .create();
+
+        //app
+        let app = TendermintValidatorApp::connect(
+            &format!("http://{}", server_address()),
+            TEST_TOKEN,
+            TEST_KEY_NAME,
+        )
+        .expect("Failed to connect");
+
+        let body = serde_json::to_string(&SignRequest {
+            input: TEST_PAYLOAD_TO_SIGN_BASE64.into(),
+        })
+        .unwrap();
+
+        let _sign_mock = mock(
+            "POST",
+            format!("/v1/transit/sign/{}", TEST_KEY_NAME).as_str(),
+        )
+        .match_header("X-Vault-Token", TEST_TOKEN)
+        .match_body(body.as_str())
+        .with_body(SIGN_RESPONSE)
+        .create();
+
+        //server call
+        let res = app.sign(&[]);
+        assert!(res.is_err());
+    }
+
+    //curl --header "X-Vault-Token: hvs.<...valid.token...>>" http://127.0.0.1:8200/v1/auth/token/lookup-self
+    const TOKEN_DATA: &str = r#"
+    {"request_id":"119fcc9e-85e2-1fcf-c2a2-96cfb20f7446","lease_id":"","renewable":false,"lease_duration":0,"data":{"accessor":"k1g6PqNWVIlKK9NDCWLiTvrG","creation_time":1661247016,"creation_ttl":2764800,"display_name":"token","entity_id":"","expire_time":"2022-09-24T09:30:16.898359776Z","explicit_max_ttl":0,"id":"hvs.CAESIEzWRWLvyYLGlYsCRI_Vt653K26b-cx_lrxBlFo3_2GBGh4KHGh2cy5GVzZ5b25nMVFpSkwzM1B1eHM2Y0ZqbXA","issue_time":"2022-08-23T09:30:16.898363509Z","meta":null,"num_uses":0,"orphan":false,"path":"auth/token/create","policies":["tmkms-transit-sign-policy"],"renewable":false,"ttl":2758823,"type":"service"},"wrap_info":null,"warnings":null,"auth":null}
+    "#;
+
+    //curl --header "X-Vault-Token: $VAULT_TOKEN" "${VAULT_ADDR}/v1/transit/keys/<signing_key_name>"
+    const READ_KEY_RESP: &str = r#"
+    {"request_id":"9cb10d0a-1877-6da5-284b-8ece4b131ae3","lease_id":"","renewable":false,"lease_duration":0,"data":{"allow_plaintext_backup":false,"auto_rotate_period":0,"deletion_allowed":false,"derived":false,"exportable":false,"imported_key":false,"keys":{"1":{"creation_time":"2022-08-23T09:30:16.676998915Z","name":"ed25519","public_key":"ng+ab41LawVupIXX3ocMn+AfV2W1DEMCfjAdtrwXND8="}},"latest_version":1,"min_available_version":0,"min_decryption_version":1,"min_encryption_version":0,"name":"cosmoshub-sign-key","supports_decryption":false,"supports_derivation":true,"supports_encryption":false,"supports_signing":true,"type":"ed25519"},"wrap_info":null,"warnings":null,"auth":null}    
+    "#;
+
+    //curl --request POST --header "X-Vault-Token: $VAULT_TOKEN" "${VAULT_ADDR}/v1/transit/sign/<..key_name...>" -d '{"input":"base64 encoded"}'
+    const SIGN_RESPONSE: &str = r#"
+    {"request_id":"13534911-8e98-9a0f-a701-e9a7736140e2","lease_id":"","renewable":false,"lease_duration":0,"data":{"key_version":1,"signature":"vault:v1:pNcc/FAUu+Ta7itVegaMUMGqXYkzE777y3kOe8AtdRTgLbA8eFnrKbbX/m7zoiC+vArsIUJ1aMCEDRjDK3ZsBg=="},"wrap_info":null,"warnings":null,"auth":null}
+    "#;
+}
