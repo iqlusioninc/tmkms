7585721a9baed1c397434224a5878758a538f409
0000 1000 1101 0010 <fffe@08d2.com>
2022-09-06 15:55:24 +0200

    Remove everything from mekatek branch

--- src/bin/tmkms/main.rs
+++
@@ -1,8 +0,0 @@
-//! Main entry point for the `tmkms` executable
-
-use tmkms::application::APP;
-
-/// Boot the `tmkms` application
-fn main() {
-    abscissa_core::boot(&APP);
-}
--- tests/cli/yubihsm/detect.rs
+++
@@ -1,9 +0,0 @@
-//! Integration tests for the `yubihsm detect` subcommand
-
-use crate::cli;
-
-#[test]
-fn detect_command_test() {
-    // TODO: parse results
-    cli::run_successfully(&["yubihsm", "detect"]);
-}
