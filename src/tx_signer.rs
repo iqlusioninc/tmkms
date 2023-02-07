e7e56862fd9d17fa732570a2a3793b7b24f7bd17
Shella Stephens <shella@iqlusion.io>
2023-02-07 07:30:49 -0700

    Update tendermint to v0.27.0 (#698)
    * tendermint* v0.27.0

--- src/tx_signer.rs
+++ src/tx_signer.rs
@@ -290,12 +290,10 @@ impl TxSigner {
     async fn broadcast_tx(&mut self, sign_msg: SignMsg, sequence: u64) -> Result<(), Error> {
         let tx = self.sign_tx(&sign_msg)?;
 
-        let amino_tx = tendermint_rpc::abci::Transaction::from(
-            tx.to_amino_bytes(self.tx_builder.schema().namespace()),
-        );
+        let amino_tx = tx.to_amino_bytes(self.tx_builder.schema().namespace());
 
         let amino_tx_hex =
-            String::from_utf8(hex::encode(amino_tx.as_ref())).expect("hex should always be UTF-8");
+            String::from_utf8(hex::encode(&amino_tx)).expect("hex should always be UTF-8");
 
         info!(
             "[{}] broadcasting TX: {}",
