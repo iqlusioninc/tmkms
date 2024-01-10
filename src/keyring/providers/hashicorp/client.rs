8d7b379662d35023331b62aed04b1a11b48ffa89
Mateusz Kaczanowski <kaczanowski.mateusz@gmail.com>
2024-01-10 06:26:24 +0100

    nits

--- src/keyring/providers/hashicorp/client.rs
+++ src/keyring/providers/hashicorp/client.rs
@@ -46,13 +46,13 @@ pub struct Root<T> {
 ///Sign Request Struct
 #[derive(Debug, Serialize)]
 struct SignRequest {
-    input: String, //Base64 encoded
+    input: String, // Base64 encoded
 }
 
 ///Sign Response Struct
 #[derive(Debug, Deserialize)]
 struct SignResponse {
-    signature: String, //Base64 encoded
+    signature: String, // Base64 encoded
 }
 
 #[derive(Debug, Serialize)]
@@ -122,10 +122,10 @@ impl std::fmt::Display for CreateKeyType {
 
 impl TendermintValidatorApp {
     pub fn connect(api_endpoint: &str, token: &str, key_name: &str) -> Result<Self, Error> {
-        //this call performs token self lookup, to fail fast
-        //let mut client = Client::new(host, token)?;
+        // this call performs token self lookup, to fail fast
+        // let mut client = Client::new(host, token)?;
 
-        //default conect timeout is 30s, this should be ok, since we block
+        // default conect timeout is 30s, this should be ok, since we block
         let agent: Agent = ureq::AgentBuilder::new()
             .timeout_read(Duration::from_secs(5))
             .timeout_write(Duration::from_secs(5))
@@ -165,8 +165,8 @@ impl TendermintValidatorApp {
         Ok(())
     }
 
-    //vault read transit/keys/cosmoshub-sign-key
-    //GET http://0.0.0.0:8200/v1/transit/keys/cosmoshub-sign-key
+    // vault read transit/keys/cosmoshub-sign-key
+    // GET http://0.0.0.0:8200/v1/transit/keys/cosmoshub-sign-key
     /// Get public key
     pub fn public_key(&mut self) -> Result<[u8; ed25519::VerifyingKey::BYTE_SIZE], Error> {
         if let Some(v) = self.public_key_value {
@@ -182,7 +182,7 @@ impl TendermintValidatorApp {
             keys: BTreeMap<usize, HashMap<String, String>>,
         }
 
-        //TODO - explore "latest"
+        // TODO - explore "latest"
         let data = if let Some(data) = self
             .agent
             .get(&format!(
@@ -201,7 +201,7 @@ impl TendermintValidatorApp {
             ));
         };
 
-        //latest key version
+        // latest key version
         let key_data = data.keys.iter().last();
 
         let pubk = if let Some((version, map)) = key_data {
@@ -245,15 +245,15 @@ impl TendermintValidatorApp {
         let mut array = [0u8; ed25519::VerifyingKey::BYTE_SIZE];
         array.copy_from_slice(&pubk[..ed25519::VerifyingKey::BYTE_SIZE]);
 
-        //cache it...
+        // cache it...
         self.public_key_value = Some(array);
         debug!("Public key: value cached {}", self.key_name,);
 
         Ok(array)
     }
 
-    //vault write transit/sign/cosmoshub-sign-key plaintext=$(base64 <<< "some-data")
-    //"https://127.0.0.1:8200/v1/transit/sign/cosmoshub-sign-key"
+    // vault write transit/sign/cosmoshub-sign-key plaintext=$(base64 <<< "some-data")
+    // "https://127.0.0.1:8200/v1/transit/sign/cosmoshub-sign-key"
     /// Sign message
     pub fn sign(&self, message: &[u8]) -> Result<[u8; ed25519::Signature::BYTE_SIZE], Error> {
         debug!("signing request: received");
@@ -292,11 +292,11 @@ impl TendermintValidatorApp {
             )));
         }
 
-        //signature: "vault:v1:/bcnnk4p8Uvidrs1/IX9s66UCOmmfdJudcV1/yek9a2deMiNGsVRSjirz6u+ti2wqUZfG6UukaoSHIDSSRV5Cw=="
+        // signature: "vault:v1:/bcnnk4p8Uvidrs1/IX9s66UCOmmfdJudcV1/yek9a2deMiNGsVRSjirz6u+ti2wqUZfG6UukaoSHIDSSRV5Cw=="
         let base64_signature = if let Some(sign) = parts.last() {
             sign.to_owned()
         } else {
-            //this should never happen
+            // this should never happen
             return Err(Error::InvalidSignature("last part is not available".into()));
         };
 
@@ -371,21 +371,21 @@ mod tests {
 
     const TEST_TOKEN: &str = "test-token";
     const TEST_KEY_NAME: &str = "test-key-name";
-    const TEST_PUB_KEY_VALUE: &str = "ng+ab41LawVupIXX3ocMn+AfV2W1DEMCfjAdtrwXND8="; //base64
-    const TEST_PAYLOAD_TO_SIGN_BASE64: &str = "cXFxcXFxcXFxcXFxcXFxcXFxcXE="; //$(base64 <<< "qqqqqqqqqqqqqqqqqqqq") => "cXFxcXFxcXFxcXFxcXFxcXFxcXEK", 'K' vs "=" ????
+    const TEST_PUB_KEY_VALUE: &str = "ng+ab41LawVupIXX3ocMn+AfV2W1DEMCfjAdtrwXND8="; // base64
+    const TEST_PAYLOAD_TO_SIGN_BASE64: &str = "cXFxcXFxcXFxcXFxcXFxcXFxcXE="; // $(base64 <<< "qqqqqqqqqqqqqqqqqqqq") => "cXFxcXFxcXFxcXFxcXFxcXFxcXEK", 'K' vs "=" ????
     const TEST_PAYLOAD_TO_SIGN: &[u8] = b"qqqqqqqqqqqqqqqqqqqq";
 
     const TEST_SIGNATURE:&str = /*vault:v1:*/ "pNcc/FAUu+Ta7itVegaMUMGqXYkzE777y3kOe8AtdRTgLbA8eFnrKbbX/m7zoiC+vArsIUJ1aMCEDRjDK3ZsBg==";
 
     #[test]
     fn hashicorp_connect_ok() {
-        //setup
+        // setup
         let lookup_self = mock("GET", "/v1/auth/token/lookup-self")
             .match_header(VAULT_TOKEN, TEST_TOKEN)
             .with_body(TOKEN_DATA)
             .create();
 
-        //test
+        // test
         let app = TendermintValidatorApp::connect(
             &format!("http://{}", server_address()),
             TEST_TOKEN,
@@ -398,13 +398,13 @@ mod tests {
 
     #[test]
     fn hashicorp_public_key_ok() {
-        //setup
+        // setup
         let lookup_self = mock("GET", "/v1/auth/token/lookup-self")
             .match_header("X-Vault-Token", TEST_TOKEN)
             .with_body(TOKEN_DATA)
             .create();
 
-        //app
+        // app
         let mut app = TendermintValidatorApp::connect(
             &format!("http://{}", server_address()),
             TEST_TOKEN,
@@ -412,17 +412,17 @@ mod tests {
         )
         .expect("Failed to connect");
 
-        //Vault call
+        // Vault call
         let read_key = mock(
             "GET",
             format!("/v1/transit/keys/{}", TEST_KEY_NAME).as_str(),
         )
         .match_header("X-Vault-Token", TEST_TOKEN)
         .with_body(READ_KEY_RESP)
-        .expect_at_most(1) //one call only
+        .expect_at_most(1) // one call only
         .create();
 
-        //server call
+        // server call
         let res = app.public_key();
         assert!(res.is_ok());
         assert_eq!(
@@ -430,7 +430,7 @@ mod tests {
             base64::decode(TEST_PUB_KEY_VALUE).unwrap().as_slice()
         );
 
-        //cached vaule
+        // cached vaule
         let res = app.public_key();
         assert!(res.is_ok());
         assert_eq!(
@@ -444,13 +444,13 @@ mod tests {
 
     #[test]
     fn hashicorp_sign_ok() {
-        //setup
+        // setup
         let lookup_self = mock("GET", "/v1/auth/token/lookup-self")
             .match_header("X-Vault-Token", TEST_TOKEN)
             .with_body(TOKEN_DATA)
             .create();
 
-        //app
+        // app
         let app = TendermintValidatorApp::connect(
             &format!("http://{}", server_address()),
             TEST_TOKEN,
@@ -472,7 +472,7 @@ mod tests {
         .with_body(SIGN_RESPONSE)
         .create();
 
-        //server call
+        // server call
         let res = app.sign(TEST_PAYLOAD_TO_SIGN);
         assert!(res.is_ok());
         assert_eq!(
@@ -486,13 +486,13 @@ mod tests {
 
     #[test]
     fn hashicorp_sign_empty_payload_should_fail() {
-        //setup
+        // setup
         let lookup_self = mock("GET", "/v1/auth/token/lookup-self")
             .match_header("X-Vault-Token", TEST_TOKEN)
             .with_body(TOKEN_DATA)
             .create();
 
-        //app
+        // app
         let app = TendermintValidatorApp::connect(
             &format!("http://{}", server_address()),
             TEST_TOKEN,
@@ -514,7 +514,7 @@ mod tests {
         .with_body(SIGN_RESPONSE)
         .create();
 
-        //server call
+        // server call
         let res = app.sign(&[]);
         assert!(res.is_err());
 
@@ -522,17 +522,17 @@ mod tests {
         sign_mock.expect(0);
     }
 
-    //curl --header "X-Vault-Token: hvs.<...valid.token...>>" http://127.0.0.1:8200/v1/auth/token/lookup-self
+    // curl --header "X-Vault-Token: hvs.<...valid.token...>>" http://127.0.0.1:8200/v1/auth/token/lookup-self
     const TOKEN_DATA: &str = r#"
     {"request_id":"119fcc9e-85e2-1fcf-c2a2-96cfb20f7446","lease_id":"","renewable":false,"lease_duration":0,"data":{"accessor":"k1g6PqNWVIlKK9NDCWLiTvrG","creation_time":1661247016,"creation_ttl":2764800,"display_name":"token","entity_id":"","expire_time":"2022-09-24T09:30:16.898359776Z","explicit_max_ttl":0,"id":"hvs.CAESIEzWRWLvyYLGlYsCRI_Vt653K26b-cx_lrxBlFo3_2GBGh4KHGh2cy5GVzZ5b25nMVFpSkwzM1B1eHM2Y0ZqbXA","issue_time":"2022-08-23T09:30:16.898363509Z","meta":null,"num_uses":0,"orphan":false,"path":"auth/token/create","policies":["tmkms-transit-sign-policy"],"renewable":false,"ttl":2758823,"type":"service"},"wrap_info":null,"warnings":null,"auth":null}
     "#;
 
-    //curl --header "X-Vault-Token: $VAULT_TOKEN" "${VAULT_ADDR}/v1/transit/keys/<signing_key_name>"
+    // curl --header "X-Vault-Token: $VAULT_TOKEN" "${VAULT_ADDR}/v1/transit/keys/<signing_key_name>"
     const READ_KEY_RESP: &str = r#"
     {"request_id":"9cb10d0a-1877-6da5-284b-8ece4b131ae3","lease_id":"","renewable":false,"lease_duration":0,"data":{"allow_plaintext_backup":false,"auto_rotate_period":0,"deletion_allowed":false,"derived":false,"exportable":false,"imported_key":false,"keys":{"1":{"creation_time":"2022-08-23T09:30:16.676998915Z","name":"ed25519","public_key":"ng+ab41LawVupIXX3ocMn+AfV2W1DEMCfjAdtrwXND8="}},"latest_version":1,"min_available_version":0,"min_decryption_version":1,"min_encryption_version":0,"name":"cosmoshub-sign-key","supports_decryption":false,"supports_derivation":true,"supports_encryption":false,"supports_signing":true,"type":"ed25519"},"wrap_info":null,"warnings":null,"auth":null}    
     "#;
 
-    //curl --request POST --header "X-Vault-Token: $VAULT_TOKEN" "${VAULT_ADDR}/v1/transit/sign/<..key_name...>" -d '{"input":"base64 encoded"}'
+    // curl --request POST --header "X-Vault-Token: $VAULT_TOKEN" "${VAULT_ADDR}/v1/transit/sign/<..key_name...>" -d '{"input":"base64 encoded"}'
     const SIGN_RESPONSE: &str = r#"
     {"request_id":"13534911-8e98-9a0f-a701-e9a7736140e2","lease_id":"","renewable":false,"lease_duration":0,"data":{"key_version":1,"signature":"vault:v1:pNcc/FAUu+Ta7itVegaMUMGqXYkzE777y3kOe8AtdRTgLbA8eFnrKbbX/m7zoiC+vArsIUJ1aMCEDRjDK3ZsBg=="},"wrap_info":null,"warnings":null,"auth":null}
     "#;
