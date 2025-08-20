179875e9996b75aefc965423631b72480436a65a
Tony Arcieri <tony@iqlusion.io>
2025-08-19 20:38:06 -0600

    tmkms-p2p: add KDF test vectors

--- tmkms-p2p/src/kdf.rs
+++ tmkms-p2p/src/kdf.rs
@@ -60,3 +60,31 @@ impl Drop for Kdf {
         self.challenge.zeroize();
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Kdf;
+    use hex_literal::hex;
+
+    /// Shared secret for Alice/Bob example exchange in `handshake.rs`
+    const SHARED_SECRET: [u8; 32] =
+        hex!("d28c3fcbdb42e28d940d3823103d379693c3724a25d96e9f0524a7306b43ca44");
+
+    const KEY1: [u8; 32] = hex!("4de5c254243a6a7b52fafd0b0c4db59175975cd435e99b7f758265aaaeeea063");
+    const KEY2: [u8; 32] = hex!("6083a1a00e5ea92cdc380c55013f3c87d87ade022666fd5aad4ae3a1530d0885");
+    const CHALLENGE: [u8; 32] =
+        hex!("cad18a7a530a6fd6e7f56e372aab9ac9410eb0ab4ca1cee89f5089e58d9e9e3e");
+
+    #[test]
+    fn kdf() {
+        let kdf1 = Kdf::derive_secrets_and_challenge(&SHARED_SECRET, false);
+        assert_eq!(kdf1.recv_secret, KEY1);
+        assert_eq!(kdf1.send_secret, KEY2);
+        assert_eq!(kdf1.challenge, CHALLENGE);
+
+        let kdf2 = Kdf::derive_secrets_and_challenge(&SHARED_SECRET, true);
+        assert_eq!(kdf2.recv_secret, KEY2);
+        assert_eq!(kdf2.send_secret, KEY1);
+        assert_eq!(kdf2.challenge, CHALLENGE);
+    }
+}
