e40b0a95a02e575e1803a6a6afb384831be04a48
Tony Arcieri (iqlusion) <tony@iqlusion.io>
2025-08-22 14:38:00 -0600

    tmkms-p2p: impl `ReadMsg`/`WriteMsg` directly on `SecretConnection` (#1048)
    The only thing tmkms cares about is reading and writing encrypted
    `proto::privval::Message` messages.
    
    That's a lot easier to support than a generic `io::{Read, Write}` where
    the caller may request arbitrary sizes at any time. And it seems that
    doing so trivially exposes bugs in the existing receive buffer
    implementation: #1046
    
    This commit does the minimum changes needed to remove
    `io::{Read, Write}` support from `tmkms-p2p`, replacing it with
    `ReadMsg`/`WriteMsg` impls. Doing so opens up all sorts of potential
    future code quality improvements, like ripping out `SecretConnection`'s
    internal buffering (which seems to be broken).

--- src/connection.rs
+++ src/connection.rs
@@ -1,14 +1,45 @@
 //! Connections to a validator (TCP or Unix socket)
 
 use self::unix::UnixConnection;
+use crate::error::Error;
 use std::io;
-use tmkms_p2p::SecretConnection;
+use tendermint_proto as proto;
+use tmkms_p2p::{ReadMsg, SecretConnection, WriteMsg};
 
 pub mod tcp;
 pub mod unix;
 
 /// Connections to a validator
-pub trait Connection: io::Read + io::Write + Sync + Send {}
+pub trait Connection: Sync + Send {
+    /// Read a request from the validator.
+    fn read_request(&mut self) -> Result<proto::privval::Message, Error>;
 
-impl<T> Connection for SecretConnection<T> where T: io::Read + io::Write + Sync + Send {}
-impl<T> Connection for UnixConnection<T> where T: io::Read + io::Write + Sync + Send {}
+    /// Write a response to the validator.
+    fn write_response(&mut self, msg: &proto::privval::Message) -> Result<(), Error>;
+}
+
+impl<T> Connection for SecretConnection<T>
+where
+    T: io::Read + io::Write + Sync + Send,
+{
+    fn read_request(&mut self) -> Result<proto::privval::Message, Error> {
+        Ok(self.read_msg()?)
+    }
+
+    fn write_response(&mut self, msg: &proto::privval::Message) -> Result<(), Error> {
+        Ok(self.write_msg(msg)?)
+    }
+}
+
+impl<T> Connection for UnixConnection<T>
+where
+    T: io::Read + io::Write + Sync + Send,
+{
+    fn read_request(&mut self) -> Result<proto::privval::Message, Error> {
+        Ok(self.read_msg()?)
+    }
+
+    fn write_response(&mut self, msg: &proto::privval::Message) -> Result<(), Error> {
+        Ok(self.write_msg(msg)?)
+    }
+}
--- tmkms-p2p/src/lib.rs
+++ tmkms-p2p/src/lib.rs
@@ -36,7 +36,13 @@ pub type PeerId = [u8; 20];
 pub(crate) use curve25519_dalek::montgomery::MontgomeryPoint as EphemeralPublic;
 pub(crate) use ed25519_dalek as ed25519;
 
-/// Maximum size of a message
+/// Limit (in bytes) on the maximum size of a length delimited message (e.g. Protobuf), where the
+/// message potentially spans multiple encrypted frames.
+///
+/// It ensures we won't allocate excessively large buffers when consuming incoming requests.
+pub(crate) const MAX_MSG_LEN: usize = 1_048_576; // 1 MiB
+
+/// Maximum size of a message frame
 pub(crate) const FRAME_MAX_SIZE: usize = 1024;
 
 /// 4 + 1024 == 1028 total frame size
@@ -46,3 +52,14 @@ pub(crate) const TOTAL_FRAME_SIZE: usize = FRAME_MAX_SIZE + LENGTH_PREFIX_SIZE;
 /// Size of the `ChaCha20Poly1305` MAC tag
 pub(crate) const TAG_SIZE: usize = 16;
 pub(crate) const TAGGED_FRAME_SIZE: usize = TOTAL_FRAME_SIZE + TAG_SIZE;
+
+/// Decode the total length of a length-delimited Protobuf or other LEB128-prefixed message,
+/// including the length of the length prefix itself (which is variable-sized).
+fn decode_length_delimiter_inclusive(frame: &[u8]) -> Result<usize> {
+    // TODO(tarcieri): would this fail on non-canonical LEB128, e.g. with leading zeros?
+    let len = prost::decode_length_delimiter(frame)?;
+    let length_delimiter_len = prost::length_delimiter_len(len);
+    length_delimiter_len
+        .checked_add(len)
+        .ok_or_else(|| prost::DecodeError::new("length overflow").into())
+}
