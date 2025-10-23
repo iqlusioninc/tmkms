b347b396cda7a737606a3c843cb6025cdffe85e6
Tony Arcieri <tony@iqlusion.io>
2025-10-23 17:20:07 -0600

    Remove `cosmrs` dependency
    To unblock the upgrade to `cometbft-rs` (#1124), this removes the only
    usage of `cosmrs`, which is for formatting Cosmos JSON public keys.

--- src/keyring/format.rs
+++ src/keyring/format.rs
@@ -1,9 +1,14 @@
 //! Chain-specific key configuration
 
-use cosmrs::crypto::PublicKey;
-use serde::Deserialize;
-use subtle_encoding::bech32;
+use serde::{Deserialize, Serialize};
+use subtle_encoding::{base64, bech32};
 use tendermint::TendermintKey;
+
+/// Protobuf [`Any`] type URL for Ed25519 public keys
+const ED25519_TYPE_URL: &str = "/cosmos.crypto.ed25519.PubKey";
+
+/// Protobuf [`Any`] type URL for secp256k1 public keys
+const SECP256K1_TYPE_URL: &str = "/cosmos.crypto.secp256k1.PubKey";
 
 /// Options for how keys for this chain are represented
 #[derive(Clone, Debug, Deserialize)]
@@ -41,11 +46,45 @@ impl Format {
                 }
                 TendermintKey::ConsensusKey(pk) => pk.to_bech32(consensus_key_prefix),
             },
-            Format::CosmosJson => PublicKey::from(*public_key.public_key()).to_json(),
+            Format::CosmosJson => {
+                let pk = match public_key {
+                    TendermintKey::AccountKey(pk) => PublicKeyJson::from(&pk),
+                    TendermintKey::ConsensusKey(pk) => PublicKeyJson::from(&pk),
+                };
+                serde_json::to_string(&pk).expect("JSON serialization error")
+            }
             Format::Hex => match public_key {
                 TendermintKey::AccountKey(pk) => pk.to_hex(),
                 TendermintKey::ConsensusKey(pk) => pk.to_hex(),
             },
         }
+    }
+}
+
+/// Serde encoding type for JSON public keys.
+///
+/// Uses Protobuf JSON encoding conventions.
+#[derive(Deserialize, Serialize)]
+struct PublicKeyJson {
+    /// `@type` field e.g. `/cosmos.crypto.ed25519.PubKey`.
+    #[serde(rename = "@type")]
+    type_url: String,
+
+    /// Key data: standard Base64 encoded with padding.
+    key: String,
+}
+
+impl From<&tendermint::PublicKey> for PublicKeyJson {
+    fn from(public_key: &tendermint::PublicKey) -> PublicKeyJson {
+        let type_url = match public_key {
+            tendermint::PublicKey::Ed25519(_) => ED25519_TYPE_URL,
+            tendermint::PublicKey::Secp256k1(_) => SECP256K1_TYPE_URL,
+            // `tendermint::PublicKey` is `non_exhaustive`
+            _ => unreachable!("unknown pubic key type"),
+        }
+        .to_owned();
+
+        let key = String::from_utf8(base64::encode(public_key.to_bytes())).expect("UTF-8 error");
+        PublicKeyJson { type_url, key }
     }
 }
